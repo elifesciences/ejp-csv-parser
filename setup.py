5a3db8b096a9b71b0b4979f3c198e02876144802
Graham Nott <gnott@starglobal.ca>
2021-09-28 17:22:11 -0700

    Jenkins release pipeline config and README.

--- setup.py
+++ setup.py
@@ -1,30 +1,26 @@
 from setuptools import setup
-
 import ejpcsvparser
 
-with open('README.rst') as fp:
+with open("README.md") as fp:
     README = fp.read()
 
 setup(
-    name='ejpcsvparser',
+    name="ejpcsvparser",
     version=ejpcsvparser.__version__,
-    description='EJP CSV parser for building article objects.',
+    description="EJP CSV parser for building article objects.",
     long_description=README,
-    packages=['ejpcsvparser'],
-    license='MIT',
-    install_requires=[
-        "elifearticle",
-        "GitPython",
-        "configparser"
-    ],
-    url='https://github.com/elifesciences/ejp-csv-parser',
-    maintainer='eLife Sciences Publications Ltd.',
-    maintainer_email='tech-team@elifesciences.org',
+    long_description_content_type="text/markdown",
+    packages=["ejpcsvparser"],
+    license="MIT",
+    install_requires=["elifearticle", "GitPython", "configparser"],
+    url="https://github.com/elifesciences/ejp-csv-parser",
+    maintainer="eLife Sciences Publications Ltd.",
+    maintainer_email="tech-team@elifesciences.org",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
-        ]
-    )
+    ],
+)
